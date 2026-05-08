8faa71314947ffd76308d1a4ed5a565a19258b77
kagonameshack-cmyk <kagonameshack@gmail.com>
2026-05-08 15:06:22 +0300

    script.js

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ window.addEventListener("scroll", () => {
     }
 
 });
+
+const menuToggle = document.getElementById("menuToggle");
+const mobileMenu = document.getElementById("mobileMenu");
+
+menuToggle.addEventListener("click", () => {
+
+    mobileMenu.classList.toggle("active");
+
+});
